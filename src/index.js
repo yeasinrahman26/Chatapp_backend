de64c2f3def76e065bd478be4fafeaa658b4d29b
yeasinrahman26 <yeasinrahmansafa@gmail.com>
2025-09-01 05:07:01 +0600

    final push

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import cors from "cors"
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
@@ -12,6 +14,12 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials:true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
